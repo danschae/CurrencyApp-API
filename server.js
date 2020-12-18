ec3b5b8dac240db119d4493c7507aba45c33dbb9
Daniel <3138740851@qq.com>
2020-12-18 10:50:17 -0500

    add database to backend

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const PORT = process.env.PORT || 8001;
 const bodyParser = require('body-parser');
 const app = require('express')();
 
 app.use(bodyParser.json());
+
+const db = require('./lib/pool.js');
 
 app.listen(PORT, () => {
   console.log('listening on ', PORT);
