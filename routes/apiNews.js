851fc5f2374b935751bc61799dccef23da5384ad
Daniel <3138740851@qq.com>
2020-12-19 15:44:44 -0500

    move news to backend

---
+++ routes/apiNews.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const router = require('express').Router();
+const NewsAPI = require('newsapi');
+console.log(process.env.DB_HOST)
+
+const today = new Date()
+const yesterday = new Date(today)
+
+yesterday.setDate(yesterday.getDate() - 1)
+
+const now = today.toDateString()
+const previousDay = yesterday.toDateString();
+
+
+const newsapi = new NewsAPI(`b2e12bf8b8c042418009597a5371cfa2`);
+
+const showNews = newsapi.v2.everything({
+    q: ['finance', 'obama'],
+    sources: ['bbc-news', 'techcrunch'],
+    domains: 'bbc.co.uk, techcrunch.com',
+    from: previousDay,
+    to: now,
+    language: 'en',
+    sortBy: 'relevancy',
+    page: 2
+  }).then(response => {
+      console.log(response.totalResults);
+  })
+    .catch(err => console.log(err));
+
+console.log(showNews)
