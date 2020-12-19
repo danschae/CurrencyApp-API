b10ff5b622503af4b48c63f12c4f7891afe7e529
Daniel <3138740851@qq.com>
2020-12-19 15:50:12 -0500

    add in different possible dates to search by

--- routes/apiNews.js
+++ routes/apiNews.js
@@ -5,20 +5,23 @@ console.log(process.env.DB_HOST)
 
 const today = new Date()
 const yesterday = new Date(today)
+const lastWeek = new Date(today)
 
 yesterday.setDate(yesterday.getDate() - 1)
+lastWeek.setDate(lastWeek.setDate() - 7)
 
 const now = today.toDateString()
 const previousDay = yesterday.toDateString();
+const previousWeek = lastWeek.toDateString();
 
 
 const newsapi = new NewsAPI(`b2e12bf8b8c042418009597a5371cfa2`);
 
 const showNews = newsapi.v2.everything({
-    q: ['finance', 'obama'],
+    q: 'finance' || 'obama',
     sources: ['bbc-news', 'techcrunch'],
     domains: 'bbc.co.uk, techcrunch.com',
-    from: previousDay,
+    from: previousWeek,
     to: now,
     language: 'en',
     sortBy: 'relevancy',
